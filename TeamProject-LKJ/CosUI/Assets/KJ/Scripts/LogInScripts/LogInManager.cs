48f19f4371c6e7ad2345b0f628145a6b2920d217
KiJun Lee <rickyjun94@gmail.com>
2024-03-18 20:18:48 +0900

    Cos 팀프로젝트 UI
    - 로그인 구현 중 ( Firebase Auth 활용)
    - 로그인 부수적인 기능 구현 예정.

--- TeamProject-LKJ/CosUI/Assets/KJ/Scripts/LogInScripts/LogInManager.cs
+++ TeamProject-LKJ/CosUI/Assets/KJ/Scripts/LogInScripts/LogInManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LogInManager : MonoBehaviour
 {
@@ -43,4 +45,71 @@ public class LogInManager : MonoBehaviour
         SceneManager.LoadSceneAsync("LoadingScene");
     }
     #endregion
+    #region 회원가입창 열고 닫기
+    [SerializeField] private GameObject _registerUI;
+
+    public void OpenRegister()
+    {
+        _registerUI.SetActive(true);
+    }
+
+    public void CloseRegister()
+    {
+        _registerUI.SetActive(false);
+    }
+    #endregion
+    #region InputField 이미지 숨기기
+    // InputField 와 Image 를 1대1 매칭하기 위한 Dictionary.
+    private Dictionary<TMP_InputField, GameObject> _inputFieldImageMap = new Dictionary<TMP_InputField, GameObject>();
+
+    [Header("ImageToHide")]
+    public TMP_InputField emailLogin;
+    public TMP_InputField passwordLogin;
+
+    public TMP_InputField usernameRegister;
+    public TMP_InputField emailRegister;
+    public TMP_InputField passwordRegister;
+    public TMP_InputField passwordCheckRegister;
+
+    [Header("Image")]
+    public GameObject emailLoginImage;
+    public GameObject passwordLoginImage;
+
+    public GameObject usernameRegisterImage;
+    public GameObject emailRegisterImage;
+    public GameObject passwordRegisterImage;
+    public GameObject passwordCheckImage;
+
+
+
+    #endregion
+
+    void Start()
+    {
+        // 각 InputField 와 Image 매칭
+        /* -----------------------------Login------------------------------- */
+        _inputFieldImageMap.Add(emailLogin, emailLoginImage);
+        _inputFieldImageMap.Add(passwordLogin, passwordLoginImage);
+        /* ---------------------------Register------------------------------ */
+        _inputFieldImageMap.Add(usernameRegister, usernameRegisterImage);
+        _inputFieldImageMap.Add(emailRegister, emailRegisterImage);
+        _inputFieldImageMap.Add(passwordRegister, passwordRegisterImage);
+        _inputFieldImageMap.Add(passwordCheckRegister, passwordCheckImage);
+
+        // 모든 InputField 에 Listener 추가.
+        foreach (var pair in _inputFieldImageMap)
+        {
+            pair.Key.onValueChanged.AddListener((value) => ToggleImage(pair.Key, value));
+        }
+    }
+        
+    void ToggleImage(TMP_InputField inputField, string inputValue)
+    {
+        // 해당 InputField 에 연결된 이미지 활성화 / 비활성화.
+        if (_inputFieldImageMap.TryGetValue(inputField, out GameObject Image))
+        {
+            Image.SetActive(string .IsNullOrEmpty(inputValue));
+        }
+    }
+
 }
